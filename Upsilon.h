9ceb7f73731535a4b6adb1a81f0b72f3a12d6c82
Kisoo Lee <kilee@cern.ch>
2019-01-16 19:52:18 +0900

    new ranges

--- Upsilon.h
+++ Upsilon.h
@@ -6,43 +6,128 @@
 //}}}
 
 //Basic info{{{
-
-//v1{{{
-Double_t multBinsArr[] = {0, 1500};
-Double_t rapBinsArr[] = {-2.4, 2.4};
-Double_t ptBinsArr[] = {0, 500};
-//}}}
-
 /*
-//v2{{{
+//v1: Integrated{{{
 Double_t multBinsArr[] = {0, 300};
 Double_t rapBinsArr[] = {-2.4, 2.4};
-Double_t ptBinsArr[] = {0.0, 2.0, 4.0, 6.0, 9.0, 12.0, 30.0};
+Double_t ptBinsArr[] = {0, 30};
 //}}}
 */
 /*
-//v3{{{
+//v2: pt distribution{{{
+Double_t multBinsArr[] = {0, 300};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0.0, 4.0, 6.0, 10.0, 30.0};
+Int_t massBinsArr[] = {0, 7, 15, 23, 26, 29, 32, 35, 56, 77, 98, 119};
+//}}}
+*/
+/*
+//v3: multiplicity distribution{{{
 Double_t multBinsArr[] = {0, 50, 80, 110, 150, 300};
 Double_t rapBinsArr[] = {-2.4, 2.4};
 Double_t ptBinsArr[] = {0, 30};
 //}}}
 */
 /*
-//v4{{{
-Double_t multBinsArr[] = {0, 300};
-Double_t rapBinsArr[] = {0.0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 2.4};
-Double_t ptBinsArr[] = {0, 30};
+//v4: low multiplicity pt distribution{{{
+Double_t multBinsArr[] = {0, 40};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 3.0, 6.0, 10.0, 30.0};
 //}}}
 */
+/*
+//v5: multiplicity 200~1500 pt distribution{{{
+Double_t multBinsArr[] = {200, 1500};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 6.0, 30.0};
+//}}}
+*/
+/*
+//v6: multiplicity 100~1500 pt distribution{{{
+Double_t multBinsArr[] = {100, 1500};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 6.0, 30.0};
+//}}}
+*/
+/*
+//v7: multiplicity 200~1500{{{
+Double_t multBinsArr[] = {200, 1500};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 6.0, 30.0};
+//}}}
+*/
+/*
+//v8: multiplicity 185~1500{{{
+Double_t multBinsArr[] = {185, 1500};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 6.0, 30.0};
+//}}}
+*/
+/*
+//v9: multiplicity 150~1500{{{
+Double_t multBinsArr[] = {150, 1500};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 6.0, 30.0};
+//}}}
+*/
+/*
+//v10: multiplicity 120~1500{{{
+Double_t multBinsArr[] = {120, 1500};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 6.0, 30.0};
+//}}}
+*/
+/*
+//v11: multiplicity 120~1500{{{
+Double_t multBinsArr[] = {120, 1500};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 4.0, 6.0, 10.0, 30.0};
+//}}}
+*/
+/*
+//v12: multiplicity 110~1500{{{
+Double_t multBinsArr[] = {110, 1500};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 4.0, 6.0, 10.0, 30.0};
+//}}}
+*/
+/*
+//v13: multiplicity 110~300, Trk pT 1~3{{{
+Double_t multBinsArr[] = {110, 300};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 4.0, 6.0, 10.0, 30.0};
+Int_t massBinsArr[] = {0, 7, 15, 23, 26, 29, 32, 35, 56, 77, 98, 119};
+//}}}
+*/
+/*
+//v14: multiplicity 110~300, Trk pT 0~3{{{
+Double_t multBinsArr[] = {110, 300};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0, 4.0, 6.0, 10.0, 30.0};
+Int_t massBinsArr[] = {0, 7, 15, 23, 26, 29, 32, 35, 56, 77, 98, 119};
+//}}}
+*/
+/*
+//v15: pt distribution, multiplicity 0~300{{{
+Double_t multBinsArr[] = {0, 300};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0.0, 4.0, 7.0, 10.0, 30.0};
+Int_t massBinsArr[] = {0, 7, 15, 23, 26, 29, 32, 35, 56, 77, 98, 119};
+//}}}
+*/
+
+//v16: pt distribution, multiplicity 110~300{{{
+Double_t multBinsArr[] = {110, 300};
+Double_t rapBinsArr[] = {-2.4, 2.4};
+Double_t ptBinsArr[] = {0.0, 4.0, 7.0, 10.0, 30.0};
+Int_t massBinsArr[] = {0, 7, 15, 23, 26, 29, 32, 35, 56, 77, 98, 119};
+//}}}
+
 
 const Int_t rap_narr = sizeof(rapBinsArr)/sizeof(double);
 const Int_t pt_narr = sizeof(ptBinsArr)/sizeof(double);
 const Int_t mult_narr = sizeof(multBinsArr)/sizeof(double);
-const Int_t Netabin = 33;
-const Int_t Nphibin = 33;
-
-const Int_t Numbin[10] = {0, 5000000, 10000000, 15000000, 20000000,
-								25000000, 30000000, 35000000, 40000000, 45000000};
+const Int_t mass_narr = sizeof(massBinsArr)/sizeof(int);
 
 const Double_t U1S_mass = 9.460;
 const Double_t U2S_mass = 10.023;
@@ -50,8 +135,10 @@ const Double_t U3S_mass = 10.355;
 const Double_t U1S_sigma = 0.00026;
 const Double_t U2S_sigma = 0.00031;
 const Double_t U3S_sigma = 0.0005;
-const Double_t peakFactor = 2.;
-const Double_t sideFactor = 2.;
+const Int_t Netabin1 = 32;
+const Int_t Netabin2 = 16;
+const Int_t Nphibin1 = 32;
+const Int_t Nphibin2 = 16;
 //}}}
 
 class DiMuon
@@ -67,7 +154,7 @@ class DiMuon
 	Double_t trk_eta, trk_pt, trk_phi;
 	Double_t weight;
 	Double_t mult;
-	Int_t Ntrg, Nass;
+	Int_t Ntrg, Nside, Nass;
 	TClonesArray* Vec_trg;
 	TClonesArray* Vec_ass;
 
@@ -79,7 +166,7 @@ class DiMuon
 		mumi_eta = -99; mumi_pt = -99; mumi_phi = -99;
 		trk_eta = -99; trk_pt = -99; trk_phi = -99;
 		weight = -99; mult = -99;
-		Ntrg = -99; Nass = -99;
+		Ntrg = -99; Nside = -99; Nass = -99;
 		Vec_trg->Clear();
 		Vec_ass->Clear();
 	}
@@ -107,6 +194,7 @@ class DiMuon
 		tout->Branch("weight", &weight);
 		tout->Branch("mult", &mult);
 		tout->Branch("Ntrg", &Ntrg);
+		tout->Branch("Nside", &Nside);
 		tout->Branch("Nass", &Nass);
 		Vec_trg = new TClonesArray("TLorentzVector", 250);
 		Vec_ass = new TClonesArray("TLorentzVector", 1500);
