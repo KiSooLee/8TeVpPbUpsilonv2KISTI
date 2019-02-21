020952c8755dffba181a584498145b6fdfbb2cbf
Kisoo Lee <kilee@cern.ch>
2019-02-21 22:12:50 +0900

    correlate candidates

---
+++ Correlation/do_Correl_bkg_pPb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Correl_bkg_pPb.C(isMC, multMin, multMax, ptMin, ptMax, rapMin, rapMax, trkptMin, trkptMax, version)
+
+#version="v11"
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+scram p CMSSW CMSSW_8_0_26_patch2
+cd CMSSW_8_0_26_patch2
+eval `scramv1 runtime -sh`
+cd -
+
+root -l -b -q 'Correl_bkg_pPb.C+(false, 110, 300, 0, 4, -2.4, 2.4, 0, 3, "v16", '$1')'
+#ls
+#root -l -b -q 'mCorrel_bkg_pPb.C+(false, 120, 1500, 10, 30, -2.4, 2.4, 1, 3, "v13", 8, 20)'
---
+++ Correlation/do_Correl_sig_pPb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Correl_sig_pPb.C(isMC, multMin, multMax, ptMin, ptMax, rapMin, rapMax, trkptMin, trkptMax, version)
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+scram p CMSSW CMSSW_8_0_26_patch2
+cd CMSSW_8_0_26_patch2
+eval `scramv1 runtime -sh`
+cd -
+
+root -l -b -q 'Correl_sig_pPb.C+(false, 110, 300, 0, 4, -2.4, 2.4, 0, 3, "v16", '$1')'
+#ls
+#root -l -b -q 'mCorrel_sig_pPb.C+(false, 120, 1500, 10, 30, -2.4, 2.4, 1, 3, "v13", 0)'
