d51112d57f80e457b8d4cf4cead7a0a288935e79
Kisoo Lee <kilee@cern.ch>
2019-06-26 15:52:12 +0900

    Gen info and multiplicity modified

--- CollectingCands/SortpPb1.C
+++ CollectingCands/SortpPb1.C
@@ -14,6 +14,7 @@
 #include <TH1.h>
 #include <TF1.h>
 #include <TSystem.h>
+
 #include "Style_Upv2.h"
 #include "Upsilon.h"
 /*
@@ -44,6 +45,7 @@ void SortpPb1(const bool isMC = false, const Int_t multMin = 0, const Int_t mult
 	TChain* tin = new TChain("hionia/myTree");
 	if(isMC)
 	{
+		
 		fname1 = "root://cms-xrdr.private.lo:2094///xrd/store/user/kilee/pPb_8TeV_OniaTrkTree/oniaTree_pPb1_MC_1S.root";
 	}
 	else
@@ -91,6 +93,19 @@ void SortpPb1(const bool isMC = false, const Int_t multMin = 0, const Int_t mult
 	Bool_t Reco_isMuTrk[MaxTrk];
 	TClonesArray* Reco_trk_4mom;
 	Reco_trk_4mom = 0;
+
+	Int_t Gen_QQ_size;
+	Int_t Gen_QQ_type[MaxQQ];
+	TClonesArray* Gen_QQ_4mom;
+	TClonesArray* Gen_QQ_mupl_4mom;
+	TClonesArray* Gen_QQ_mumi_4mom;
+	Gen_QQ_4mom = 0;
+	Gen_QQ_mupl_4mom = 0;
+	Gen_QQ_mumi_4mom = 0;
+	Int_t Gen_trk_size;
+	Int_t Gen_trk_charge[MaxTrk];
+	TClonesArray* Gen_trk_4mom;
+	Gen_trk_4mom = 0;
 //}}}
 
 //Branches{{{
@@ -124,6 +139,15 @@ void SortpPb1(const bool isMC = false, const Int_t multMin = 0, const Int_t mult
 	TBranch* b_Reco_isgoodTrk;
 	TBranch* b_Reco_isMuTrk;
 	TBranch* b_Reco_trk_4mom;
+
+	TBranch* b_Gen_QQ_size;
+	TBranch* b_Gen_QQ_type;
+	TBranch* b_Gen_QQ_4mom;
+	TBranch* b_Gen_QQ_mupl_4mom;
+	TBranch* b_Gen_QQ_mumi_4mom;
+	TBranch* b_Gen_trk_size;
+	TBranch* b_Gen_trk_charge;
+	TBranch* b_Gen_trk_4mom;
 //}}}
 
 //Branche address{{{
@@ -157,6 +181,16 @@ void SortpPb1(const bool isMC = false, const Int_t multMin = 0, const Int_t mult
 	tin->SetBranchAddress("Reco_isgoodTrk", Reco_isgoodTrk, &b_Reco_isgoodTrk);
 	tin->SetBranchAddress("Reco_isMuTrk", Reco_isMuTrk, &b_Reco_isMuTrk);
 	tin->SetBranchAddress("Reco_trk_4mom", &Reco_trk_4mom, &b_Reco_trk_4mom);
+	if(isMC)
+	{
+		tin->SetBranchAddress("Gen_QQ_size", &Gen_QQ_size, &b_Gen_QQ_size);
+		tin->SetBranchAddress("Gen_QQ_type", &Gen_QQ_type, &b_Gen_QQ_type);
+		tin->SetBranchAddress("Gen_QQ_4mom", &Gen_QQ_4mom, &b_Gen_QQ_4mom);
+		tin->SetBranchAddress("Gen_QQ_mupl_4mom", &Gen_QQ_mupl_4mom, &b_Gen_QQ_mupl_4mom);
+		tin->SetBranchAddress("Gen_QQ_mumi_4mom", &Gen_QQ_mumi_4mom, &b_Gen_QQ_mumi_4mom);
+		tin->SetBranchAddress("Gen_trk_size", &Gen_trk_size, &b_Gen_trk_size);
+		tin->SetBranchAddress("Gen_trk_4mom", &Gen_trk_4mom, &b_Gen_trk_4mom);
+	}
 //}}}
 
 //Define LorentzVector{{{
@@ -168,46 +202,102 @@ void SortpPb1(const bool isMC = false, const Int_t multMin = 0, const Int_t mult
 	mupl_Reco_4mom = 0;
 	mumi_Reco_4mom = 0;
 	Trk_Reco_4mom = 0;
+
+	TLorentzVector* Up_Gen_4mom = new TLorentzVector;
+	TLorentzVector* mupl_Gen_4mom = new TLorentzVector;
+	TLorentzVector* mumi_Gen_4mom = new TLorentzVector;
+	TLorentzVector* Trk_Gen_4mom = new TLorentzVector;
+	Up_Gen_4mom = 0;
+	mupl_Gen_4mom = 0;
+	mumi_Gen_4mom = 0;
+	Trk_Gen_4mom = 0;
 //}}}
 
 //initialize dataset{{{
 	DiMuon DMset;
-
-	TTree* tout = new TTree("UpsilonTree", "");
+	TTree* tout= new TTree("UpsilonTree", "");
 	tout->SetMaxTreeSize(10000000000000);
 	DMset.BuildBranch(tout);
 //}}}
 
 	const Int_t Nevt = tin->GetEntries();
-	Int_t EventNb = 0;
 	Float_t vz = -99.;
 
 	TFile* fout = new TFile(Form("%d-%d_%d-%d_%d-%d_%d-%d_pPb1/Sort_OniaTree_pPb1_PADoubleMuon_%s_%d.root", (int)multMin, (int)multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, MorD.Data(), imass), "RECREATE");
 
 	for(Int_t ievt = 0; ievt < Nevt; ievt++)
+	//for(Int_t ievt = 0; ievt < 100000; ievt++)
 	{
 		if(ievt%100000 == 0) cout << "Events: " << ievt << " / " << Nevt << " [" << Form("%.1f", ((double)(ievt)/(double)(Nevt)*100)) << " %]" << endl;
 		tin->GetEntry(ievt);
-
-		DMset.clear();
-		Int_t Ntrg = 0;
-		Int_t Nass = 0;
-
-		if( (HLTriggers&1)!=1 ) continue;
 
 //Get track multiplicity{{{
 		Int_t Tot_Ntrk = 0;
 		for(Int_t itrk = 0; itrk < Reco_trk_size; itrk++)
 		{
-			if( Reco_isgoodTrk[itrk] && !Reco_isMuTrk[itrk] ) Tot_Ntrk++;
+			if( Reco_isgoodTrk[itrk] && !Reco_isMuTrk[itrk] )
+			{
+				Trk_Reco_4mom = (TLorentzVector*) Reco_trk_4mom->At(itrk);
+				if(Trk_Reco_4mom->Pt() > 0.4) Tot_Ntrk++;
+			}
 		}
 //}}}
 
-		Bool_t is_inMass = false;
-		Bool_t has_Asso = false;
+		DMset.clear();
+
+		Int_t NtrgReco = 0;
+		Int_t NassReco = 0;
+		Bool_t is_inMassReco = false;
+		Bool_t has_AssoReco = false;
+
+		Int_t NtrgGen = 0;
+		Int_t NassGen = 0;
+		Bool_t is_inMassGen = false;
+		Bool_t has_AssoGen = false;
 
 		if(Tot_Ntrk >= multMin && Tot_Ntrk < multMax)
 		{
+			if(isMC)
+			{
+//Get trigger vector{{{
+				for(Int_t iqq = 0; iqq < Gen_QQ_size; iqq++)
+				{
+					Up_Gen_4mom = (TLorentzVector*) Gen_QQ_4mom->At(iqq);
+					mupl_Gen_4mom = (TLorentzVector*) Gen_QQ_mupl_4mom->At(iqq);
+					mumi_Gen_4mom = (TLorentzVector*) Gen_QQ_mumi_4mom->At(iqq);
+
+					if( Up_Gen_4mom->M() > 8+(0.05*imass) && Up_Gen_4mom->M() <= 8+(0.05*(imass+1)) && Up_Gen_4mom->Rapidity() >= -2.4 && Up_Gen_4mom->Rapidity() <= 2.4 && Up_Gen_4mom->Pt() >= ptMin && Up_Gen_4mom->Pt() < ptMax)
+					{
+						if( !InAcc(mupl_Gen_4mom->Pt(), mupl_Gen_4mom->Eta()) ) continue;
+						if( !InAcc(mumi_Gen_4mom->Pt(), mumi_Gen_4mom->Eta()) ) continue;
+
+						new( (*DMset.Vec_trg_Gen)[NtrgGen] )TLorentzVector(*Up_Gen_4mom);
+						NtrgGen++;
+						is_inMassGen = true;
+					}
+				}
+//}}}
+
+//Get associator vector{{{
+				if(is_inMassGen)
+				{
+					for(Int_t itrk = 0; itrk < Gen_trk_size; itrk++)
+					{
+						Trk_Gen_4mom = (TLorentzVector*) Gen_trk_4mom->At(itrk);
+						TVector3 Trk_vector;
+
+						if(Trk_Gen_4mom->Eta() <= 2.4 && Trk_Gen_4mom->Eta() >= -2.4 && Trk_Gen_4mom->Pt() >= TrkptMin && Trk_Gen_4mom->Pt() < TrkptMax && Trk_Gen_4mom->Pt() >= 0.4)
+						{
+							new( (*DMset.Vec_ass_Gen)[NassGen] )TLorentzVector(*Trk_Gen_4mom);
+							NassGen++;
+						}
+					}
+				}
+//}}}
+			}
+
+			if( (HLTriggers&1)!=1 ) continue;
+
 //Get trigger vector{{{
 			for(Int_t iqq = 0; iqq < Reco_QQ_size; iqq++)
 			{
@@ -238,43 +328,50 @@ void SortpPb1(const bool isMC = false, const Int_t multMin = 0, const Int_t mult
 					if( Reco_QQ_sign[iqq] != 0 ) continue;
 //}}}
 
-					new( (*DMset.Vec_trg)[Ntrg] )TLorentzVector(*Up_Reco_4mom);
-					Ntrg++;
-					is_inMass = true;
+					new( (*DMset.Vec_trg_Reco)[NtrgReco] )TLorentzVector(*Up_Reco_4mom);
+					NtrgReco++;
+					is_inMassReco = true;
 				}
 			}
 //}}}
 
 //Get associator vector{{{
-			if(is_inMass)
+			if(is_inMassReco)
 			{
 				for(Int_t itrk = 0; itrk < Reco_trk_size; itrk++)
 				{
 					Trk_Reco_4mom = (TLorentzVector*) Reco_trk_4mom->At(itrk);
 					TVector3 Trk_vector;
 	
-					if(Trk_Reco_4mom->Eta() <= 2.4 && Trk_Reco_4mom->Eta() >= -2.4 && Reco_isgoodTrk[itrk] && !Reco_isMuTrk[itrk] && Trk_Reco_4mom->Pt() >= TrkptMin && Trk_Reco_4mom->Pt() < TrkptMax)
+					if(Trk_Reco_4mom->Eta() <= 2.4 && Trk_Reco_4mom->Eta() >= -2.4 && Reco_isgoodTrk[itrk] && !Reco_isMuTrk[itrk] && Trk_Reco_4mom->Pt() >= TrkptMin && Trk_Reco_4mom->Pt() < TrkptMax && Trk_Reco_4mom->Pt() > 0.4)
 					{
-						new( (*DMset.Vec_ass)[Nass] )TLorentzVector(*Trk_Reco_4mom);
-						Nass++;
+						new( (*DMset.Vec_ass_Reco)[NassReco] )TLorentzVector(*Trk_Reco_4mom);
+						NassReco++;
 					}
 				}
-				if(Nass != 0) EventNb++;
 				vz = zVtx;
 			}
 //}}}
 		}
-		if(is_inMass && (Nass != 0))
+		if(isMC)
 		{
-			DMset.eventNb = EventNb;
+			if( is_inMassGen && (NassGen != 0) ) 
+			{
+				DMset.Ntrg_Gen = NtrgGen;
+				DMset.Nass_Gen = NassGen;
+			}
+		}
+		if( is_inMassReco && (NassReco != 0) )
+		{
 			DMset.zVtx = vz;
-			DMset.Ntrg = Ntrg;
-			DMset.Nass = Nass;
+			DMset.Ntrg_Reco = NtrgReco;
+			DMset.Nass_Reco = NassReco;
+		}
+		if( (is_inMassGen && (NassGen != 0)) || (is_inMassReco && (NassReco != 0)) )
+		{
 			DMset.mult = Tot_Ntrk;
-			DMset.weight = 1.;
 			tout->Fill();
 		}
-		//else DMset.mult = -99;
 	}
 	tout->Write();
 	fout->Close();
