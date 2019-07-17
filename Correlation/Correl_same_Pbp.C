44f603cfe62e722f259f5cd91457138c9ef91aa0
Kisoo Lee <kilee@cern.ch>
2019-07-17 16:19:06 +0900

    working percentage output changed

--- Correlation/Correl_same_Pbp.C
+++ Correlation/Correl_same_Pbp.C
@@ -186,7 +186,7 @@ void Correl_same_Pbp(const bool isMC = false, const Int_t multMin = 0, const Int
 
 	for(Int_t ievt = 0; ievt < Nevt; ievt++)
 	{
-		if(ievt%100 == 0) cout << "Events: " << ievt << " / " << Nevt << " [" << Form("%.1f", ((double)ievt/(double)Nevt)*100) << " %]" << endl;
+		if(ievt%1000 == 0) cout << "Events: " << ievt << " / " << Nevt << " [" << Form("%.1f", ((double)ievt/(double)Nevt)*100) << " %]" << endl;
 		tin1->GetEntry(ievt);
 
 		if(isMC)
