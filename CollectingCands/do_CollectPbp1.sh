e6c9dea83f601a0f3833d2544cf50b6ed18b2a6f
Kisoo Lee <kilee@cern.ch>
2019-07-24 15:19:34 +0900

    RECO, GEN separated

--- CollectingCands/do_CollectPbp1.sh
+++ CollectingCands/do_CollectPbp1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#Collect_Pbp1.C(isMC, multMin, multMax, ptMin, ptMax, rapMin, rapMax, trkptMin, trkptMax, MupT, imass)
+#Collect_Reco_Pbp1.C(isMC, multMin, multMax, ptMin, ptMax, rapMin, rapMax, trkptMin, trkptMax, MupT, imass)
+#Collect_Gen_Pbp1.C(multMin, multMax, ptMin, ptMax, rapMin, rapMax, trkptMin, trkptMax, MupT, imass)
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scram p CMSSW CMSSW_8_0_26_patch2
@@ -7,5 +8,7 @@ cd CMSSW_8_0_26_patch2
 eval `scramv1 runtime -sh`
 cd -
 
-root -l -b -q 'Collect_Pbp1.C+(false, 0, 40, 10, 30, -2.4, 2.4, 0, 3, "4", '$1')'
-#root -l -b -q 'Collect_Pbp1.C+(false, 0, 40, 10, 30, -2.4, 2.4, 0, 3, "4", 115)'
+root -l -b -q 'Collect_Reco_Pbp1.C+(false, 110, 300, 10, 30, -2.4, 2.4, 0, 3, "4", '$1')'
+#root -l -b -q 'Collect_Gen_Pbp1.C+(0, 40, 10, 30, -2.4, 2.4, 0, 3, "4", '$1')'
+#root -l -b -q 'Collect_Reco_Pbp1.C+(false, 110, 300, 10, 30, -2.4, 2.4, 0, 3, "4", 0)'
+#root -l -b -q 'Collect_Gen_Pbp1.C+(0, 40, 10, 30, -2.4, 2.4, 0, 3, "4", 0)'
