9cc63e1bbaa0ea1a7e88450c61458c0d7a970858
Kisoo Lee <kilee@cern.ch>
2020-06-17 14:57:35 +0900

    check version for track pt cut is added

--- Projection/do_projection.sh
+++ Projection/do_projection.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#DrawCorrel.C(isMC, isGen, isTrk, multMin, multMax, ptMin, ptMax, rapMin, rapMax, TrkptMin, TrkptMax, version, MupT)
+#DrawCorrel.C(isMC, isGen, isTrk, multMin, multMax, ptMin, ptMax, rapMin, rapMax, TrkptMin, TrkptMax, version, Weight, MupT)
 #RatioNProjection.C(isMC, isGen, isTrk, multMin, multMax, ptMin, ptMax, rapMin, rapMax, TrkptMin, TrkptMax, version, MupT)
 
 #multMin=("0" "40" "100" "200" "300")
@@ -10,12 +10,33 @@
 #rapMax=("-12" "0" "12" "24")
 #TrkptMin=("0" "1" "2" "3" "4" "6")
 #TrkptMax=("1" "2" "3" "4" "6" "10")
-multMin=("110")
+#multMin=("110")
+#multMax=("300")
+#multMin=("120")
+#multMax=("300")
+multMin=("100")
 multMax=("300")
 #multMin=("0")
 #multMax=("40")
+#multMin=("0")
+#multMax=("45")
+#multMin=("0")
+#multMax=("50")
+#multMin=("0")
+#multMax=("55")
+#multMin=("0")
+#multMax=("60")
+#multMin=("0")
+#multMin=("40")
+#multMax=("90")
+#multMin=("90")
+#multMax=("110")
 ptMin=("0" "4" "7" "10")
 ptMax=("4" "7" "10" "30")
+#ptMin=("0" "4" "7")
+#ptMax=("4" "7" "10")
+#ptMin=("0")
+#ptMax=("30")
 rapMin=("-2.4")
 rapMax=("2.4")
 TrkptMin=("0")
@@ -29,8 +50,11 @@ do
 		do
 			for((l=0; l<"${#TrkptMin[@]}"; l++))
 			do
-				root -l -b -q 'DrawCorrel.C+(false, false, true, '${multMin[$i]}', '${multMax[$i]}', '${ptMin[$j]}', '${ptMax[$j]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', "v19", true, "3p5")'
-				root -l -b -q 'RatioNProjection.C+(false, false, true, '${multMin[$i]}', '${multMax[$i]}', '${ptMin[$j]}', '${ptMax[$j]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', "v19", true, "3p5")'
+				root -l -b -q 'DrawCorrel.C+(false, false, false, '${multMin[$i]}', '${multMax[$i]}', '${ptMin[$j]}', '${ptMax[$j]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', "v36", true, "3p5", "v1")'
+				#root -l -b -q 'ForAN.C+(false, false, false, '${multMin[$i]}', '${multMax[$i]}', '${ptMin[$j]}', '${ptMax[$j]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', "v28", true, "3p5")'
+				root -l -b -q 'RatioNProjection.C+(false, false, false, '${multMin[$i]}', '${multMax[$i]}', '${ptMin[$j]}', '${ptMax[$j]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', "v36", true, "3p5", "v1")'
+				#root -l -b -q 'RatioNProjection_jet.C+(false, false, '${multMin[$i]}', '${multMax[$i]}', '${ptMin[$j]}', '${ptMax[$j]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', "v31", true, "3p5", true)'
+				#root -l -b -q 'RatioNProjection_jet.C+(false, false, '${multMin[$i]}', '${multMax[$i]}', '${ptMin[$j]}', '${ptMax[$j]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', "v31", true, "3p5", false)'
 			done
 		done
 	done
