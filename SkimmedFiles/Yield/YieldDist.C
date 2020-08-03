bbe184fca5d1411c9aed1e859b18f98b890286e9
Kisoo Lee <kilee@cern.ch>
2020-08-03 16:46:28 +0900

    input output name changed

--- SkimmedFiles/Yield/YieldDist.C
+++ SkimmedFiles/Yield/YieldDist.C
@@ -13,9 +13,9 @@
 #include "../../Headers/Style_Upv2.h"
 //}}}
 
-void YieldDist(TString version = "v1", TString MupT = "4")
+void YieldDist(const TString version = "v1", const Bool_t Weight = true, const Bool_t isAccRW = true, const Bool_t isEffRW = true, const Bool_t isTnP = true, const Bool_t SigSys = false, const Bool_t BkgSys = false, const TString MupT = "4")
 {
-	gStyle->SetOptStat(0000);
+	SetStyle();
 
 //Define canvas and histograms{{{
 	TCanvas* c1S = new TCanvas("c1S", "", 0, 0, 600, 600);
@@ -29,7 +29,8 @@ void YieldDist(TString version = "v1", TString MupT = "4")
 	TH1D* h1S, * h1SN;
 	TH1D* h2S, * h2SN;
 	TH1D* h3S, * h3SN;
-	if(mult_narr > 2)
+/*
+	if(mult_narr > 3)
 	{
 		h1S = new TH1D("h1S", ";multiplicity;Counts", mult_narr-1, multBinsArr);
 		h2S = new TH1D("h2S", ";multiplicity;Counts", mult_narr-1, multBinsArr);
@@ -38,15 +39,17 @@ void YieldDist(TString version = "v1", TString MupT = "4")
 		h2SN = new TH1D("h2SN", ";multiplicity;Counts", mult_narr-1, multBinsArr);
 		h3SN = new TH1D("h3SN", ";multiplicity;Counts", mult_narr-1, multBinsArr);
 	}
-	else if(pt_narr > 2)
-	{
+*/
+	//else if(pt_narr > 2)
+	//{
 		h1S = new TH1D("h1S", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
 		h2S = new TH1D("h2S", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
 		h3S = new TH1D("h3S", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
 		h1SN = new TH1D("h1SN", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
 		h2SN = new TH1D("h2SN", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
 		h3SN = new TH1D("h3SN", ";p_{T} (GeV/c);Counts", pt_narr-1, ptBinsArr);
-	}
+//	}
+/*
 	else if(rap_narr > 2)
 	{
 		h1S = new TH1D("h1S", ";y;Counts", rap_narr-1, rapBinsArr);
@@ -56,6 +59,7 @@ void YieldDist(TString version = "v1", TString MupT = "4")
 		h2SN = new TH1D("h2SN", ";y;Counts", rap_narr-1, rapBinsArr);
 		h3SN = new TH1D("h3SN", ";y;Counts", rap_narr-1, rapBinsArr);
 	}
+*/
 	FormTH1Marker(h1S, 0, 1, 1);
 	FormTH1Marker(h2S, 1, 1, 1);
 	FormTH1Marker(h3S, 2, 1, 1);
@@ -88,7 +92,9 @@ void YieldDist(TString version = "v1", TString MupT = "4")
 		{
 			for(Int_t irap = 0; irap < rap_narr-1; irap++)
 			{
-				TFile* fin  = new TFile(Form("Yield_Mult_%d-%d_pt_%d-%d_rap_%d-%d_%s_MupT%s.root", (int)multBinsArr[imult], (int)multBinsArr[imult+1], (int)ptBinsArr[ipt], (int)ptBinsArr[ipt+1], (int)(10*rapBinsArr[irap]), (int)(10*rapBinsArr[irap+1]), version.Data(), MupT.Data()), "READ");
+				TFile* fin;
+				if(Weight) fin  = new TFile(Form("Yield_Mult_%d-%d_pt_%d-%d_rap_%d-%d_Data_%s_Acc%o_Eff%o_TnP%o_SigSys%o_BkgSys%o_MupT%s.root", multBinsArr[imult], multBinsArr[imult+1], (int)ptBinsArr[ipt], (int)ptBinsArr[ipt+1], (int)(10*rapBinsArr[irap]), (int)(10*rapBinsArr[irap+1]), version.Data(), isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT.Data()), "READ");
+				else fin  = new TFile(Form("Yield_Mult_%d-%d_pt_%d-%d_rap_%d-%d_Data_%s_noWeight_MupT%s.root", multBinsArr[imult], multBinsArr[imult+1], (int)ptBinsArr[ipt], (int)ptBinsArr[ipt+1], (int)(10*rapBinsArr[irap]), (int)(10*rapBinsArr[irap+1]), version.Data(), MupT.Data()), "READ");
 				TH1D* hin = (TH1D*) fin->Get("hYield");
 
 				Double_t w1S = 0;
@@ -172,7 +178,8 @@ void YieldDist(TString version = "v1", TString MupT = "4")
 	lt1->DrawLatex(0.54,0.63,Form("%d < p_{T}^{#Upsilon} < %d GeV/c", (int)ptBinsArr[0], (int)ptBinsArr[pt_narr-1]));
 	lt1->DrawLatex(0.54,0.56,Form("%d < mult. < %d", (int)multBinsArr[0], (int)multBinsArr[mult_narr-1]));
 	lt1->DrawLatex(0.54,0.49,"|y^{#Upsilon}| < 2.4");
-	c1S->SaveAs(Form("Yield_distribution_1S_%s_MupT%s.pdf", version.Data(), MupT.Data()));
+	if(Weight) c1S->SaveAs(Form("Yield_distribution_Data_1S_%s_Acc%o_Eff%o_TnP%o_SigSys%o_BkgSys%o_MupT%s.pdf", version.Data(), isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT.Data()));
+	else c1S->SaveAs(Form("Yield_distribution_Data_1S_%s_noWeight_MupT%s.pdf", version.Data(), MupT.Data()));
 
 	c1SN->cd();
 	h1SN->Draw();
@@ -183,23 +190,28 @@ void YieldDist(TString version = "v1", TString MupT = "4")
 	lt1N->DrawLatex(0.54,0.63,Form("%d < p_{T}^{#Upsilon} < %d GeV/c", (int)ptBinsArr[0], (int)ptBinsArr[pt_narr-1]));
 	lt1N->DrawLatex(0.54,0.56,Form("%d < mult. < %d", (int)multBinsArr[0], (int)multBinsArr[mult_narr-1]));
 	lt1N->DrawLatex(0.54,0.49,"|y^{#Upsilon}| < 2.4");
-	c1SN->SaveAs(Form("Yield_distribution_1SN_%s_MupT%s.pdf", version.Data(), MupT.Data()));
+	if(Weight) c1SN->SaveAs(Form("Yield_distribution_Data_1SN_%s_Acc%o_Eff%o_TnP%o_SigSys%o_BkgSys%o_MupT%s.pdf", version.Data(), isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT.Data()));
+	else c1SN->SaveAs(Form("Yield_distribution_Data_1SN_%s_noWeight_MupT%s.pdf", version.Data(), MupT.Data()));
 
 	c2S->cd();
 	h2S->Draw("petext0");
-	c2S->SaveAs(Form("Yield_distribution_2S_%s_MupT%s.pdf", version.Data(), MupT.Data()));
+	if(Weight) c2S->SaveAs(Form("Yield_distribution_Data_2S_%s_Acc%o_Eff%o_TnP%o_SigSys%o_BkgSys%o_MupT%s.pdf", version.Data(), isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT.Data()));
+	else c2S->SaveAs(Form("Yield_distribution_Data_2S_%s_noWeight_MupT%s.pdf", version.Data(), MupT.Data()));
 
 	c2SN->cd();
 	h2SN->Draw();
-	c2SN->SaveAs(Form("Yield_distribution_2SN_%s_MupT%s.pdf", version.Data(), MupT.Data()));
+	if(Weight) c2SN->SaveAs(Form("Yield_distribution_Data_2SN_%s_Acc%o_Eff%o_TnP%o_SigSys%o_BkgSys%o_MupT%s.pdf", version.Data(), isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT.Data()));
+	else c2SN->SaveAs(Form("Yield_distribution_Data_2SN_%s_noWeight_MupT%s.pdf", version.Data(), MupT.Data()));
 
 	c3S->cd();
 	h3S->Draw("petext0");
-	c3S->SaveAs(Form("Yield_distribution_3S_%s_MupT%s.pdf", version.Data(), MupT.Data()));
+	if(Weight) c3S->SaveAs(Form("Yield_distribution_Data_3S_%s_Acc%o_Eff%o_TnP%o_SigSys%o_BkgSys%o_MupT%s.pdf", version.Data(), isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT.Data()));
+	else c3S->SaveAs(Form("Yield_distribution_Data_3S_%s_noWeight_MupT%s.pdf", version.Data(), MupT.Data()));
 
 	c3SN->cd();
 	h3SN->Draw();
-	c3SN->SaveAs(Form("Yield_distribution_3SN_%s_MupT%s.pdf", version.Data(), MupT.Data()));
+	if(Weight) c3SN->SaveAs(Form("Yield_distribution_Data_3SN_%s_Acc%o_Eff%o_TnP%o_SigSys%o_BkgSys%o_MupT%s.pdf", version.Data(), isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT.Data()));
+	else c3SN->SaveAs(Form("Yield_distribution_Data_3SN_%s_noWeight_MupT%s.pdf", version.Data(), MupT.Data()));
 
 	call->cd();
 	h1S->Draw();
@@ -215,7 +227,8 @@ void YieldDist(TString version = "v1", TString MupT = "4")
 	lt1N->DrawLatex(0.54,0.59,Form("%d < p_{T}^{#Upsilon} < %d GeV/c", (int)ptBinsArr[0], (int)ptBinsArr[pt_narr-1]));
 	lt1N->DrawLatex(0.54,0.52,Form("%d < mult. < %d", (int)multBinsArr[0], (int)multBinsArr[mult_narr-1]));
 	lt1N->DrawLatex(0.54,0.45,"|y^{#Upsilon}| < 2.4");
-	call->SaveAs(Form("Yield_distribution_all_%s_MupT%s.pdf", version.Data(), MupT.Data()));
+	if(Weight) call->SaveAs(Form("Yield_distribution_Data_all_%s_Acc%o_Eff%o_TnP%o_SigSys%o_BkgSys%o_MupT%s.pdf", version.Data(), isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT.Data()));
+	else call->SaveAs(Form("Yield_distribution_Data_all_%s_noWeight_MupT%s.pdf", version.Data(), MupT.Data()));
 
 	callN->cd();
 	h1SN->Draw();
@@ -231,5 +244,6 @@ void YieldDist(TString version = "v1", TString MupT = "4")
 	lt1N->DrawLatex(0.54,0.59,Form("%d < p_{T}^{#Upsilon} < %d GeV/c", (int)ptBinsArr[0], (int)ptBinsArr[pt_narr-1]));
 	lt1N->DrawLatex(0.54,0.52,Form("%d < mult. < %d", (int)multBinsArr[0], (int)multBinsArr[mult_narr-1]));
 	lt1N->DrawLatex(0.54,0.45,"|y^{#Upsilon}| < 2.4");
-	callN->SaveAs(Form("Yield_distribution_allN_%s_MupT%s.pdf", version.Data(), MupT.Data()));
+	if(Weight) callN->SaveAs(Form("Yield_distribution_Data_allN_%s_Acc%o_Eff%o_TnP%o_SigSys%o_BkgSys%o_MupT%s.pdf", version.Data(), isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT.Data()));
+	else callN->SaveAs(Form("Yield_distribution_Data_allN_%s_noWeight_MupT%s.pdf", version.Data(), MupT.Data()));
 }
