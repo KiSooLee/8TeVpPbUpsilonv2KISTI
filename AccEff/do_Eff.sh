d3ea7cb9b59f3f77de134074a17a6d10d18708b5
Kisoo Lee <kilee@cern.ch>
2020-08-05 18:36:34 +0900

    new macro added

--- AccEff/do_Eff.sh
+++ AccEff/do_Eff.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #Eff.C(Generation, MupT, isRW, isTnP)
+#DrawEff.C(Generation, MupT, isRW, isTnP)
+#DrawAccEff.C(Generation, MupT)
 
-root -l -b -q 'Eff.C+(1, "3p5", false, true)'
-root -l -b -q 'Eff.C+(1, "3p5", true, true)'
-root -l -b -q 'Eff.C+(1, "3p5", true, false)'
-#root -l -b -q 'DrawEff.C+(1, "3p5", true)'
+#root -l -b -q 'Eff.C+(1, "3p5", false, true)'
+#root -l -b -q 'Eff.C+(1, "3p5", true, true)'
+#root -l -b -q 'Eff.C+(1, "3p5", true, false)'
+#root -l -b -q 'DrawEff.C+(1, "3p5", false, true)'
+#root -l -b -q 'DrawEff.C+(1, "3p5", true, true)'
+#root -l -b -q 'DrawEff.C+(1, "3p5", true, false)'
+#root -l -b -q 'DrawAccEff.C+(1, "3p5")'
