36e8866e7e76216690d5d44bfba1da7c547fb084
Kisoo Lee <kilee@cern.ch>
2020-08-05 20:00:22 +0900

    Acceptance reweighting effect check

--- AccEff/RWCompAcc.C
+++ AccEff/RWCompAcc.C
@@ -59,8 +59,8 @@ void RWCompAcc(const Int_t Generation = 1, const TString MupT = "3p5")
 	TFile* fRW1 = new TFile(Form("Plots/AccPlots_Upsilon_%dS_RW1_MupT%s.root", Generation, MupT.Data()), "READ");
 	TH1D* hRW0 = (TH1D*) fRW0->Get("hAccPt");
 	TH1D* hRW1 = (TH1D*) fRW1->Get("hAccPt");
-	FormTH1Marker(hRW, 0, 0, 2.0);
-	FormTH1Marker(hRW, 1, 0, 2.0);
+	FormTH1Marker(hRW0, 0, 1, 1.6);
+	FormTH1Marker(hRW1, 1, 0, 1.6);
 
 	TCanvas* c1 = new TCanvas("c1", "", 0, 0, 600, 800);
 	c1->cd();
@@ -77,26 +77,30 @@ void RWCompAcc(const Int_t Generation = 1, const TString MupT = "3p5")
 	hRW0->SetMinimum(0.0);
 	hRW0->Draw("pe");
 	hRW1->Draw("pesame");
-	TLegend* leg1 = new TLegend(0.5, 0.5, 0.9, 0.8);
+	TLegend* leg1 = new TLegend(0.15, 0.7, 0.5, 0.88);
 	FormLegend(leg1, 0.045);
 	leg1->AddEntry(hRW0, "Non-weighted", "pe");
 	leg1->AddEntry(hRW1, "dN/dp_{T} weighted", "pe");
 	leg1->Draw();
 	TLatex* lt1 = new TLatex();
 	FormLatex(lt1, 12, 0.045);
-	lt1->DrawLatex(0.7, 0.82, Form("p_{T}^{#mu} > %.1f GeV/c", MupTCut));
-	lt1->DrawLatex(0.7, 0.75, Form("|#eta^{#mu}| < 2.4"));
+	lt1->DrawLatex(0.62, 0.82, Form("p_{T}^{#mu} > %.1f GeV/c", MupTCut));
+	lt1->DrawLatex(0.62, 0.75, Form("|#eta^{#mu}| < 2.4"));
 
 	pad_ratio->cd();
 	TH1D* hratio = (TH1D*) hRW1->Clone("hratio");
-	hratio->Divide(hRW0);
+	hratio->Divide(hratio, hRW0, 1, 1, "B");
 	hratio->GetYaxis()->SetTitle("Ratio");
 	hratio->GetYaxis()->SetTitleSize(0.15);
 	hratio->GetYaxis()->SetTitleOffset(0.2);
 	hratio->GetXaxis()->SetTitleSize(0.1);
 	hratio->GetXaxis()->SetTitleOffset(0.8);
+	hratio->SetMaximum(1.01);
+	hratio->SetMinimum(0.99);
 	hratio->Draw("pe");
-	lt1->DrawLatex(0.4, 0.82, "Acc.dN/dp_{T} weighted/ Acc. non-weighted");
+	TLatex* lt2 = new TLatex();
+	FormLatex(lt2, 12, 0.12);
+	lt2->DrawLatex(0.3, 0.8, "Acc.dN/dp_{T} weighted/ Acc. non-weighted");
 	SetLine(2, 0, 1, 30, 1, 0, 3);
-	c1->SaveAs(Form("Plots/Acc_comp_Upsilon_%dS_RW%o_MupT%s.pdf", Generation, isRW, MupT.Data()));
+	c1->SaveAs(Form("Plots/Acc_RWcomp_Upsilon_%dS_MupT%s.pdf", Generation, MupT.Data()));
 }
