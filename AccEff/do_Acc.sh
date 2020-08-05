c2dbf43281b2a9f2662350e3296a2f27a6b49f3e
Kisoo Lee <kilee@cern.ch>
2020-08-05 20:05:28 +0900

    file range modified

--- AccEff/do_Acc.sh
+++ AccEff/do_Acc.sh
@@ -3,11 +3,10 @@
 #DrawAcc.C(Generation, MupT, isRW)
 #RWCompAcc.C(Generation, MupT)
 
-#for((i=1; i<25; i++))
-#do
-#	root -l -b -q 'Acc.C+(1, "3p5", true, '$i')'
-#	root -l -b -q 'Acc.C+(1, "3p5", false, '$i')'
-#done
+for((i=1; i<26; i++))
+do
+	root -l -b -q 'Acc.C+(1, "3p5", true, '$i')'
+	root -l -b -q 'Acc.C+(1, "3p5", false, '$i')'
+done
 #root -l -b -q 'DrawAcc.C+(1, "3p5", true)'
-root -l 'DrawAcc.C+(1, "3p5", true)'
 #root -l -b -q 'RWCompAcc.C+(1, "3p5")'
