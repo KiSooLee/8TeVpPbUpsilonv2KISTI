45fe9781c972adff83a58591271615eece0baab2
Kisoo Lee <kilee@cern.ch>
2020-08-17 16:30:12 +0900

    sequence changed

--- SubtractTrk/Subtract_Trk.C
+++ SubtractTrk/Subtract_Trk.C
@@ -19,19 +19,14 @@
 #include "../Headers/Style_Upv2.h"
 //}}}
 
-void Subtract_Trk(const Bool_t isMC = false, const Int_t multMin = 0, const Int_t multMax = 300, const Double_t ptMin = 0, const Double_t ptMax = 30, const Double_t rapMin = -2.4, const Double_t rapMax = 2.4, const Double_t TrkptMin = 0, const Double_t TrkptMax = 1, const Int_t maxF = 2, const TString version = "v13", const TString MupT = "4", const bool Weight = true)
+void Subtract_Trk(const Bool_t isMC = false, const Int_t multMinhi = 0, const Int_t multMaxhi = 300, const Int_t multMinlow = 0, const Int_t multMaxlow = 50, const Double_t rapMin = -2.4, const Double_t rapMax = 2.4, const Double_t TrkptMin = 0, const Double_t TrkptMax = 1, const Int_t maxF = 2, const Int_t bkgN = 0, const Bool_t isfine = true, const TString versionhi = "v56", const TString versionlow = "v55", const TString Jversionhi = "v47", const TString Jversionlow = "v41", const Bool_t isAccRW = true, const Bool_t isEffRW = true, const Bool_t isTnP = true, const TString MupT = "4")
 {
 	SetStyle();
-	gStyle->SetOptFit(0000);
 
 //set fitting condition name{{{
-	TString Away[3];
-	Away[0] = "1";
-	Away[1] = "1p5";
-	Away[2] = "2";
-	TString Fine[2];
-	Fine[0] = "fine";
-	Fine[1] = "coarse";
+	TString Fine;
+	if(isfine) Fine = "fine";
+	else Fine = "coarse";
 	TString MorD;
 	if(isMC) MorD = "MC";
 	else MorD = "Data";
@@ -47,105 +42,76 @@ void Subtract_Trk(const Bool_t isMC = false, const Int_t multMin = 0, const Int_
 
 //Make directory{{{
 	TString mainDIR = gSystem->ExpandPathName(gSystem->pwd());
-	TString fDIR = mainDIR + Form("/V2File/%s", version.Data());
-	TString aDIR[3];
-	TString pDIR[3];
+	TString aDIR = mainDIR + Form("/V2Plot/MupT%s", MupT.Data());
 
-	for(Int_t iaway = 0; iaway < 3; iaway++)
-	{
-		aDIR[iaway] = mainDIR + Form("/V2Plot/%s/Away%s", version.Data(), Away[iaway].Data());
-		pDIR[iaway] = mainDIR + Form("/V2Plot/%s/Away%s/MupT%s", version.Data(), Away[iaway].Data(), MupT.Data());
-	}
-
-	void * dirf = gSystem->OpenDirectory(fDIR.Data());
-	if(dirf) gSystem->FreeDirectory(dirf);
-	else gSystem->mkdir(fDIR.Data(), kTRUE);
-
-	for(Int_t iaway = 0; iaway < 3; iaway++)
-	{
-		void * dira = gSystem->OpenDirectory(aDIR[iaway].Data());
-		if(dira) gSystem->FreeDirectory(dira);
-		else gSystem->mkdir(aDIR[iaway].Data(), kTRUE);
-
-		void * dirp = gSystem->OpenDirectory(pDIR[iaway].Data());
-		if(dirp) gSystem->FreeDirectory(dirp);
-		else gSystem->mkdir(pDIR[iaway].Data(), kTRUE);
-	}
+	void * dira = gSystem->OpenDirectory(aDIR.Data());
+	if(dira) gSystem->FreeDirectory(dira);
+	else gSystem->mkdir(aDIR.Data(), kTRUE);
 //}}}
 
 //Define canvas for yield and vn dist{{{
 	TCanvas* c1 = new TCanvas("c1", "", 0, 0, 600, 600);
-	TH1D* hist;
-	if(isMC) hist = new TH1D("hist", "", 20, 8.5, 10.);
-	else hist = new TH1D("hist", "", 20, 8., 14.);
+	TH1D* hist = new TH1D("hist", "", 30, 0, 30);
 	FormTH1Marker(hist, 0, 0, 1.4);
-	hist->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})");
-	hist->GetYaxis()->SetTitle("v_{2}^{S+B}");
-	hist->SetMinimum(-0.2);
-	hist->SetMaximum(1.0);
+	hist->GetXaxis()->SetTitle("p_{T} (GeV/c)");
+	hist->GetYaxis()->SetTitle("v_{2}");
+	hist->SetMinimum(-0.05);
+	hist->SetMaximum(0.15);
 //}}}
 
-	TFile* fout;
-	fout = new TFile(Form("V2File/%s/v2_sig_Reco_Mult_%d-%d_pt_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_%s_MupT%s_weight%o%s.root", version.Data(), multMin, multMax, (int)ptMin, (int)ptMax, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, MorD.Data(), version.Data(), MupT.Data(), Weight, Ffit.Data()), "RECREATE");
+	TFile* fout = new TFile(Form("V2Plot/MupT%s/Final_v2_Reco_Mult_%d-%d_by_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_%s_by_%s_jet_%s_by_%s_Acc%o_Eff%o_TnP%o_MupT%s.root", MupT.Data(), multMinhi, multMaxhi, multMinlow, multMaxlow, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, MorD.Data(), versionhi.Data(), versionlow.Data(), Jversionhi.Data(), Jversionlow.Data(), isAccRW, isEffRW, isTnP, MupT.Data()), "RECREATE");
 
 //Get vn file{{{
 	TFile* fsigtrk;
 	TFile* ftrktrk;
-	fsigtrk = new TFile(Form("../ExtractV2/ProjDist/DistFiles/%s/MupT%s/v2_dist_Reco_Mult_%d-%d_pt_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_%s_MupT%s_weight%o%s.root", version.Data(), MupT.Data(), multMin, multMax, (int)ptMin, (int)ptMax, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, MorD.Data(), version.Data(), MupT.Data(), Weight, Ffit.Data()), "READ");
-	ftrktrk = new TFile(Form("../ExtractV2/ProjDist/DistFiles/%s/MupT%s/trk_v2_dist_Reco_Mult_%d-%d_pt_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_%s_MupT%s_weight%o%s.root", version.Data(), MupT.Data(), multMin, multMax, (int)ptMin, (int)ptMax, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, MorD.Data(), version.Data(), MupT.Data(), Weight, Ffit.Data()), "READ");
+	fsigtrk = new TFile(Form("../LMsub/File/Subv2_Mult_%d-%d_by_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_by_%s_jet_%s_by_%s_Acc%o_Eff%o_TnP%o_MupT%s.root", multMinhi, multMaxhi, multMinlow, multMaxlow, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, versionhi.Data(), versionlow.Data(), Jversionhi.Data(), Jversionlow.Data(), isAccRW, isEffRW, isTnP, MupT.Data()), "READ");
+	ftrktrk = new TFile(Form("../LMsub/File/trk_Subv2_Mult_%d-%d_by_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_by_%s_jet_%s_by_%s_MupT%s.root", multMinhi, multMaxhi, multMinlow, multMaxlow, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, versionhi.Data(), versionlow.Data(), Jversionhi.Data(), Jversionlow.Data(), MupT.Data()), "READ");
 //}}}
 
-	for(Int_t ifine = 0; ifine < 2; ifine++)
-	{
-		for(Int_t iaway = 0; iaway < 3; iaway++)
-		{
 //calculate v2 value{{{
-			TGraphErrors* gsigtrk = (TGraphErrors*) fsigtrk->Get(Form("gv2_Away%s_%s", Away[iaway].Data(), Fine[ifine].Data()));
-			TGraphErrors* gtrktrk = (TGraphErrors*) ftrktrk->Get(Form("gv2_Away%s_%s", Away[iaway].Data(), Fine[ifine].Data()));
+	TGraphErrors* gsigtrk = (TGraphErrors*) fsigtrk->Get(Form("gv2sub"));
+	TGraphErrors* gtrktrk = (TGraphErrors*) ftrktrk->Get(Form("gv2sub"));
 
-			Double_t v2xsigtrk[200];
-			Double_t v2ysigtrk[200];
-			Double_t v2xtrktrk[200];
-			Double_t v2ytrktrk[200];
-			Double_t Exsigtrk[200];
-			Double_t Eysigtrk[200];
-			Double_t Extrktrk[200];
-			Double_t Eytrktrk[200];
-			Double_t v2ysig[200];
-			Double_t Eysig[200];
+	Double_t v2xsigtrk[200];
+	Double_t v2ysigtrk[200];
+	Double_t v2xtrktrk[200];
+	Double_t v2ytrktrk[200];
+	Double_t Exsigtrk[200];
+	Double_t Eysigtrk[200];
+	Double_t Extrktrk[200];
+	Double_t Eytrktrk[200];
+	Double_t v2ysig[200];
+	Double_t Eysig[200];
 
-			for(Int_t ibin = 0; ibin < gsigtrk->GetN(); ibin++)
-			{
-				gsigtrk->GetPoint(ibin, v2xsigtrk[ibin], v2ysigtrk[ibin]);
-				gtrktrk->GetPoint(ibin, v2xtrktrk[ibin], v2ytrktrk[ibin]);
-				Exsigtrk[ibin] = gsigtrk->GetErrorX(ibin);
-				Extrktrk[ibin] = gtrktrk->GetErrorX(ibin);
-				Eysigtrk[ibin] = gsigtrk->GetErrorY(ibin);
-				Eytrktrk[ibin] = gtrktrk->GetErrorY(ibin);
+	for(Int_t ibin = 0; ibin < gsigtrk->GetN(); ibin++)
+	{
+		gsigtrk->GetPoint(ibin, v2xsigtrk[ibin], v2ysigtrk[ibin]);
+		gtrktrk->GetPoint(ibin, v2xtrktrk[ibin], v2ytrktrk[ibin]);
+		Exsigtrk[ibin] = gsigtrk->GetErrorX(ibin);
+		Extrktrk[ibin] = gtrktrk->GetErrorX(ibin);
+		Eysigtrk[ibin] = gsigtrk->GetErrorY(ibin);
+		Eytrktrk[ibin] = gtrktrk->GetErrorY(ibin);
 
-				if(v2ytrktrk[ibin] > 0) v2ysig[ibin] = v2ysigtrk[ibin]/TMath::Sqrt(v2ytrktrk[ibin]);
-				else if(v2ytrktrk[ibin] < 0) v2ysig[ibin] = v2ysigtrk[ibin]/TMath::Sqrt(fabs(v2ytrktrk[ibin]));
-				else v2ysig[ibin] = v2ysigtrk[ibin];
-/*
-				if(ptMin == 4 && ibin == 0) Eysig[ibin] = 0.1;
-				else Eysig[ibin] = fabs(v2ysig[ibin])*TMath::Sqrt( TMath::Power( Eysigtrk[ibin]/v2ysigtrk[ibin], 2 )+TMath::Power( Eytrktrk[ibin]/(2*v2ytrktrk[ibin]), 2 ) );
-*/
-				Eysig[ibin] = fabs(v2ysig[ibin])*TMath::Sqrt( TMath::Power( Eysigtrk[ibin]/v2ysigtrk[ibin], 2 )+TMath::Power( Eytrktrk[ibin]/(2*v2ytrktrk[ibin]), 2 ) );
-			}
+		if(v2ytrktrk[ibin] > 0) v2ysig[ibin] = v2ysigtrk[ibin]/TMath::Sqrt(v2ytrktrk[ibin]);
+		else if(v2ytrktrk[ibin] < 0) v2ysig[ibin] = v2ysigtrk[ibin]/TMath::Sqrt(fabs(v2ytrktrk[ibin]));
+		else v2ysig[ibin] = v2ysigtrk[ibin];
 
-			TGraphErrors* gv2 = new TGraphErrors(gsigtrk->GetN(), v2xsigtrk, v2ysig, Exsigtrk, Eysig);
-			gv2->SetName(Form("gv2_Away%s_%s", Away[iaway].Data(), Fine[ifine].Data()));
-			gv2->SetMarkerStyle(20);
+		//if(ptMin == 4 && ibin == 0) Eysig[ibin] = 0.1;
+		//else Eysig[ibin] = fabs(v2ysig[ibin])*TMath::Sqrt( TMath::Power( Eysigtrk[ibin]/v2ysigtrk[ibin], 2 )+TMath::Power( Eytrktrk[ibin]/(2*v2ytrktrk[ibin]), 2 ) );
 
-			TCanvas* c1 = new TCanvas("c1", "", 0, 0, 600, 600);
-			c1->cd();
-			hist->Draw();
-			gv2->Draw("samepe");
-			c1->SaveAs(Form("V2Plot/%s/Away%s/MupT%s/Sigv2_dist_%s_Mult_%d-%d_pt_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_%s_MupT%s_weight%o%s.pdf", version.Data(), Away[iaway].Data(), MupT.Data(), Fine[ifine].Data(), multMin, multMax, (int)ptMin, (int)ptMax, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, MorD.Data(), version.Data(), MupT.Data(), Weight, Ffit.Data()));
-
-			fout->cd();
-			gv2->Write();
-//}}}
-		}
+		Eysig[ibin] = fabs(v2ysig[ibin])*TMath::Sqrt( TMath::Power( Eysigtrk[ibin]/v2ysigtrk[ibin], 2 )+TMath::Power( Eytrktrk[ibin]/(2*v2ytrktrk[ibin]), 2 ) );
 	}
+
+	TGraphErrors* gv2 = new TGraphErrors(gsigtrk->GetN(), v2xsigtrk, v2ysig, Exsigtrk, Eysig);
+	gv2->SetName(Form("gv2"));
+	gv2->SetMarkerStyle(20);
+
+	c1->cd();
+	hist->Draw();
+	gv2->Draw("samepe");
+	c1->SaveAs(Form("V2Plot/MupT%s/Final_v2_dist_Reco_Mult_%d-%d_by_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_%s_by_%s_jet_%s_by_%s_Acc%o_Eff%o_TnP%o_MupT%s.pdf", MupT.Data(), multMinhi, multMaxhi, multMinlow, multMaxlow, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, MorD.Data(), versionhi.Data(), versionlow.Data(), Jversionhi.Data(), Jversionlow.Data(), isAccRW, isEffRW, isTnP, MupT.Data()));
+	fout->cd();
+	gv2->Write();
+	fout->Close();
+//}}}
 }
