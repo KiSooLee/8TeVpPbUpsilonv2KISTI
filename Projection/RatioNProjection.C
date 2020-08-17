6f3db2934d2e1fbb376e766f021a3559a25300ca
Kisoo Lee <kilee@cern.ch>
2020-08-17 16:12:15 +0900

    track correlation option changed

--- Projection/RatioNProjection.C
+++ Projection/RatioNProjection.C
@@ -25,19 +25,9 @@
 #include "../Headers/Upsilon.h"
 //}}}
 
-void RatioNProjection(const Bool_t isMC = false, const Bool_t isGen = false, const Bool_t isTrk = false, const Int_t multMin = 0, const Int_t multMax = 300, const Double_t ptMin = 0, const Double_t ptMax = 30, const Double_t rapMin = -2.4, const Double_t rapMax = 2.4, const Double_t TrkptMin = 0, const Double_t TrkptMax = 1, const TString version = "v1", const bool Weight = true, const TString MupT = "4", const TString trkptversion = "v1")
+void RatioNProjection(const Bool_t isMC = false, const Bool_t isGen = false, const Bool_t isTrk = false, const Int_t multMin = 0, const Int_t multMax = 300, const Double_t ptMin = 0, const Double_t ptMax = 30, const Double_t rapMin = -2.4, const Double_t rapMax = 2.4, const Double_t TrkptMin = 0, const Double_t TrkptMax = 1, const TString version = "v1", const TString trkptversion = "v1", const Bool_t isAccRW = true, const Bool_t isEffRW = true, const Bool_t isTnP = true, const TString MupT = "4")
 { 
 	SetStyle();
-	gStyle->SetOptFit(0);
-
-//Make directory{{{
-	TString mainDIR = gSystem->ExpandPathName(gSystem->pwd());
-	TString fileDIR = mainDIR + Form("/CorrDist/CorrFiles/%s/MupT%s", version.Data(), MupT.Data());
-
-	void * dirfile = gSystem->OpenDirectory(fileDIR.Data());
-	if(dirfile) gSystem->FreeDirectory(dirfile);
-	else gSystem->mkdir(fileDIR.Data(), kTRUE);
-//}}}
 
 //Define names{{{
 	TString MorD;
@@ -52,6 +42,25 @@ void RatioNProjection(const Bool_t isMC = false, const Bool_t isGen = false, con
 	TString Trk;
 	if(isTrk) Trk = "trk_";
 	else Trk = "";
+	TString SysDir;
+	if( isMC || (!isMC && isAccRW && isEffRW && isTnP) ) SysDir = "Nominal";
+	else if(!isMC && !isAccRW && isEffRW && isTnP) SysDir = "SysAcc";
+	else if(!isMC && isAccRW && !isEffRW && isTnP) SysDir = "SysEff";
+	else if(!isMC && isAccRW && isEffRW && !isTnP) SysDir = "SysTnP";
+	else
+	{
+		cout << "There is no such option" << endl;
+		return;
+	}
+//}}}
+
+//Make directory{{{
+	TString mainDIR = gSystem->ExpandPathName(gSystem->pwd());
+	TString fileDIR = mainDIR + Form("/CorrDist/CorrFiles/%s/MupT%s/%s", version.Data(), MupT.Data(), SysDir.Data());
+
+	void * dirfile = gSystem->OpenDirectory(fileDIR.Data());
+	if(dirfile) gSystem->FreeDirectory(dirfile);
+	else gSystem->mkdir(fileDIR.Data(), kTRUE);
 //}}}
 
 	TFile *samePbp[2];
@@ -71,13 +80,27 @@ void RatioNProjection(const Bool_t isMC = false, const Bool_t isGen = false, con
 
 	for(Int_t imass = 0; imass < mass_narr-1; imass++)
 	{
-		TFile* fout = new TFile(Form("CorrDist/CorrFiles/%s/MupT%s/%sdphi_distribution_%s_Mult_%d-%d_pt_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_%s_weight%o_MupT%s_%d.root", version.Data(), MupT.Data(), Trk.Data(), RorG.Data(), multMin, multMax, (int)ptMin, (int)ptMax, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, MorD.Data(), version.Data(), Weight, MupT.Data(), imass), "RECREATE");
+		TFile* fout;
+		if(isTrk)
+		{
+			if(imass == 0) fout = new TFile(Form("CorrDist/CorrFiles/%s/MupT%s/%s/%sdphi_distribution_%s_Mult_%d-%d_pt_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_%s_MupT%s.root", version.Data(), MupT.Data(), SysDir.Data(), Trk.Data(), RorG.Data(), multMin, multMax, (int)ptMin, (int)ptMax, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, MorD.Data(), version.Data(), MupT.Data()), "RECREATE");
+			else continue;
+		}
+		else fout = new TFile(Form("CorrDist/CorrFiles/%s/MupT%s/%s/%sdphi_distribution_%s_Mult_%d-%d_pt_%d-%d_rap_%d-%d_Trkpt_%d-%d_%s_%s_Acc%o_Eff%o_TnP%o_MupT%s_%d.root", version.Data(), MupT.Data(), SysDir.Data(), Trk.Data(), RorG.Data(), multMin, multMax, (int)ptMin, (int)ptMax, (int)(10*rapMin), (int)(10*rapMax), (int)TrkptMin, (int)TrkptMax, MorD.Data(), version.Data(), isAccRW, isEffRW, isTnP, MupT.Data(), imass), "RECREATE");
 
 //Get files{{{
 		for(Int_t ipPb = 0; ipPb < 2; ipPb++)
 		{
-			samePbp[ipPb] = new TFile(Form("../Correlation/%d-%d_%d-%d_%d-%d_%d-%d_%s_MupT%s_trk%s/%sdeta-dphi_%s_%s_distribution_same_%s_weight%o_%d.root", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, version.Data(), MupT.Data(), trkptversion.Data(), Trk.Data(), RorG.Data(), Direction[ipPb].Data(), MorD.Data(), Weight, imass), "READ");
-			mixPbp[ipPb] = new TFile(Form("../Correlation/%d-%d_%d-%d_%d-%d_%d-%d_%s_MupT%s_trk%s/%sdeta-dphi_%s_%s_distribution_mix_%s_weight%o_%d.root", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, version.Data(), MupT.Data(), trkptversion.Data(), Trk.Data(), RorG.Data(), Direction[ipPb].Data(), MorD.Data(), Weight, imass), "READ");
+			if(isTrk)
+			{
+				samePbp[ipPb] = new TFile(Form("../Correlation/%d-%d_%d-%d_%d-%d_%d-%d_%s_MupT%s_trk%s/%sdeta-dphi_%s_%s_distribution_same_%s.root", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, version.Data(), MupT.Data(), trkptversion.Data(), Trk.Data(), RorG.Data(), Direction[ipPb].Data(), MorD.Data()), "READ");
+				mixPbp[ipPb] = new TFile(Form("../Correlation/%d-%d_%d-%d_%d-%d_%d-%d_%s_MupT%s_trk%s/%sdeta-dphi_%s_%s_distribution_mix_%s.root", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, version.Data(), MupT.Data(), trkptversion.Data(), Trk.Data(), RorG.Data(), Direction[ipPb].Data(), MorD.Data()), "READ");
+			}
+			else
+			{
+				samePbp[ipPb] = new TFile(Form("../Correlation/%d-%d_%d-%d_%d-%d_%d-%d_%s_MupT%s_trk%s/%sdeta-dphi_%s_%s_distribution_same_%s_Acc%o_Eff%o_TnP%o_%d.root", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, version.Data(), MupT.Data(), trkptversion.Data(), Trk.Data(), RorG.Data(), Direction[ipPb].Data(), MorD.Data(), isAccRW, isEffRW, isTnP, imass), "READ");
+				mixPbp[ipPb] = new TFile(Form("../Correlation/%d-%d_%d-%d_%d-%d_%d-%d_%s_MupT%s_trk%s/%sdeta-dphi_%s_%s_distribution_mix_%s_Acc%o_Eff%o_TnP%o_%d.root", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, version.Data(), MupT.Data(), trkptversion.Data(), Trk.Data(), RorG.Data(), Direction[ipPb].Data(), MorD.Data(), isAccRW, isEffRW, isTnP, imass), "READ");
+			}
 		}
 //}}}
 
@@ -103,6 +126,16 @@ void RatioNProjection(const Bool_t isMC = false, const Bool_t isGen = false, con
 			hMixPbp_fine[iaway][0]->Add(hMixPbp_fine[iaway][1]);
 			hMixPbp_coarse[iaway][0]->Add(hMixPbp_coarse[iaway][1]);
 //}}}
+		}
+
+		//self normalize factor of mixed event
+		Double_t NormFactor_fine = hMixPbp_fine[3][0]->GetBinContent(hMixPbp_fine[3][0]->FindBin(0, 0));
+		Double_t NormFactor_coarse = hMixPbp_coarse[3][0]->GetBinContent(hMixPbp_coarse[3][0]->FindBin(0, 0));
+
+		for(Int_t iaway = 0; iaway < Naway; iaway++)
+		{
+			hMixPbp_fine[iaway][0]->Scale(1./NormFactor_fine);
+			hMixPbp_coarse[iaway][0]->Scale(1./NormFactor_coarse);
 
 //projection{{{
 			hSameDeltaPhi_fine[iaway] = (TH1D*)hSamePbp_fine[iaway][0]->ProjectionY();
