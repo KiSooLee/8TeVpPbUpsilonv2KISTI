2935a60a6f9b4e58c2c01d302cd38f4703052ceb
Kisoo Lee <kilee@cern.ch>
2020-12-01 22:54:56 +0900

    free parameter macro added

--- SignalV2/do_Simul.sh
+++ SignalV2/do_Simul.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SimultaneousFit.C(multMin, multMax, rapMin, rapMax, TrkptMin , TrkptMax, maxF, bkgN, NU, detacut, isfine, version, MupT)
+#SimultaneousFitData.C(isTrk, multMin, multMax, rapMin, rapMax, TrkptMin , TrkptMax, maxF, bkgN, detacut, isfine, version, MupT, Weight)
 #maxF = 2, 3
-#bkgN = 0: expo*err, 1: pol2
+#bkgN = 0: expo*err, 1: pol1, 2: pol2, 3: pol3
 #NU = 1: 1S, 2: 12S, 3: 123S
 #detacut = 1: 1, 2: 1.5, 3: 2
-version="v17"
+version="v55"
 
 #multMin=("0" "40" "100" "200" "300")
 #multMax=("40" "100" "200" "300" "1500")
@@ -14,8 +14,22 @@ version="v17"
 #rapMax=("-12" "0" "12" "24")
 #TrkptMin=("0" "1" "2" "3" "4" "6")
 #TrkptMax=("1" "2" "3" "4" "6" "10")
-multMin=("110")
-multMax=("300")
+#multMin=("110")
+#multMin=("120")
+#multMin=("100")
+#multMax=("300")
+#multMin=("90")
+#multMax=("300")
+multMin=("0")
+multMax=("50")
+#multMin=("0")
+#multMax=("45")
+#multMin=("0")
+#multMax=("50")
+#multMin=("0")
+#multMax=("55")
+#multMin=("0")
+#multMax=("60")
 rapMin=("-2.4")
 rapMax=("2.4")
 TrkptMin=("0")
@@ -27,14 +41,23 @@ do
 	do
 		for((l=0; l<"${#TrkptMin[@]}"; l++))
 		do
-			root -l -b -q 'SimultaneousFit.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 2, 1, 3, 1, true, '\"$version\"', "3p5")'
-			root -l -b -q 'SimultaneousFit.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 1, 3, 1, true, '\"$version\"', "3p5")'
-			root -l -b -q 'SimultaneousFit.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 2, 1, 3, 1, false, '\"$version\"', "3p5")'
-			root -l -b -q 'SimultaneousFit.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 1, 3, 1, false, '\"$version\"', "3p5")'
-			root -l -b -q 'ObservedV2fit.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 2, 1, 3, 1, true, '\"$version\"', "3p5")'
-			root -l -b -q 'ObservedV2fit.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 1, 3, 1, true, '\"$version\"', "3p5")'
-			root -l -b -q 'ObservedV2fit.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 2, 1, 3, 1, false, '\"$version\"', "3p5")'
-			root -l -b -q 'ObservedV2fit.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 1, 3, 1, false, '\"$version\"', "3p5")'
+			#root -l 'SimultaneousFitData.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 2, 1, false, '\"$version\"', "3p5", true, true, true, false, false)'
+			#root -l 'SimultaneousFitData.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 3, 1, false, '\"$version\"', "3p5", true, true, true, false, false)'
+			#root -l 'SimultaneousFitData.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 2, 1, false, '\"$version\"', "3p5", false, true, true, false, false)'
+			#root -l 'SimultaneousFitData.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 2, 1, false, '\"$version\"', "3p5", true, false, true, false, false)'
+			#root -l 'SimultaneousFitData.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 2, 1, false, '\"$version\"', "3p5", true, true, false, false, false)'
+			#root -l 'SimultaneousFitData.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 2, 1, false, '\"$version\"', "3p5", true, true, true, true, false)'
+			#root -l 'SimultaneousFitData.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', 3, 2, 1, false, '\"$version\"', "3p5", true, true, true, false, true)'
+
+			root -l 'FreePar.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', false, '\"$version\"', "3p5", 0)'
+			root -l 'FreePar.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', false, '\"$version\"', "3p5", 1)'
+			root -l 'FreePar.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', false, '\"$version\"', "3p5", 2)'
+			root -l 'FreePar.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', false, '\"$version\"', "3p5", 3)'
+			root -l 'FreePar.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', false, '\"$version\"', "3p5", 4)'
+			root -l 'FreePar.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', false, '\"$version\"', "3p5", 5)'
+			root -l 'FreePar.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', false, '\"$version\"', "3p5", 6)'
+			root -l 'FreePar.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', false, '\"$version\"', "3p5", 7)'
+			root -l 'FreePar.C+('${multMin[$i]}', '${multMax[$i]}', '${rapMin[$k]}', '${rapMax[$k]}', '${TrkptMin[$l]}', '${TrkptMax[$l]}', false, '\"$version\"', "3p5", 8)'
 		done
 	done
 done
