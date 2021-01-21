026ca5b72b789229fb9b2a192c47f8722a938b12
Kisoo Lee <kilee@cern.ch>
2021-01-21 15:08:38 +0900

    case added

--- SkimmedFiles/Yield/do_YieldDist.sh
+++ SkimmedFiles/Yield/do_YieldDist.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 #YieldDist.C(version, Weight, isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT)
 
-version="v57"
-root -l -b -q 'YieldDist.C+('\"$version\"', false, false, false, false, false, false, "3p5")'
+version="v56"
+root -l -b -q 'YieldDist.C+('\"$version\"', true, true, true, true, false, false, "3p5")'
+root -l -b -q 'YieldDist.C+('\"$version\"', true, true, true, true, true, false, "3p5")'
+root -l -b -q 'YieldDist.C+('\"$version\"', true, true, true, true, false, true, "3p5")'
+root -l -b -q 'YieldDist.C+('\"$version\"', true, false, true, true, false, false, "3p5")'
+root -l -b -q 'YieldDist.C+('\"$version\"', true, true, false, true, false, false, "3p5")'
+root -l -b -q 'YieldDist.C+('\"$version\"', true, true, true, false, false, false, "3p5")'
+
+########## for re-weighting ##########
+#root -l -b -q 'YieldDist.C+('\"$version\"', false, false, false, false, false, false, "3p5")'
