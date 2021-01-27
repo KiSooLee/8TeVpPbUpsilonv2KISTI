fdce10ece560bde30bbee2f4fdbc158d05581178
Kisoo Lee <kilee@cern.ch>
2021-01-27 22:08:41 +0900

    mc case added

--- do_skim.sh
+++ do_skim.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #dataskim.C(isMC, Generation, Weight, isAccRW, isEffRW, isTnP, MupT)
 
-root -l -b -q 'dataskim.C+(true, 1, false, true, true, true, "3p5")'
-#root -l -b -q 'dataskim.C+(false, 1, false, true, true, true, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, true, true, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, false, true, true, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, false, true, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, true, false, "3p5")'
+root -l -b -q 'dataskim.C+(true, 1, true, true, true, false, "3p5")'
