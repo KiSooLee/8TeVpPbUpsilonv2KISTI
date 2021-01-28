4c3a22cfee85f5e8ea6039e40c437d276baac2c6
Kisoo Lee <kilee@cern.ch>
2021-01-28 14:44:54 +0900

    sample updated

--- AccEff/do_Acc.sh
+++ AccEff/do_Acc.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#Acc.C(Generation, MupT, isRW)
+#Acc.C(Generation, MupT, isRW, ifile)
 #DrawAcc.C(Generation, MupT, isRW)
 #RWCompAcc.C(Generation, MupT)
 
-for((i=1; i<26; i++))
-do
-	root -l -b -q 'Acc.C+(1, "3p5", true, '$i')'
-	root -l -b -q 'Acc.C+(1, "3p5", false, '$i')'
-done
-#root -l -b -q 'DrawAcc.C+(1, "3p5", true)'
+#for((i=1; i<26; i++))
+#do
+#	root -l -b -q 'Acc.C+(1, "3p5", true, '$i')'
+#	root -l -b -q 'Acc.C+(1, "3p5", false, '$i')'
+#done
+root -l -b -q 'DrawAcc.C+(1, "3p5", true)'
+root -l -b -q 'DrawAcc.C+(1, "3p5", false)'
 #root -l -b -q 'RWCompAcc.C+(1, "3p5")'
