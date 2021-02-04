244c46b85fbca3615bf78db6415862211f5b2d77
Kisoo Lee <kilee@cern.ch>
2021-02-04 12:54:01 +0900

    track pt updated

--- CollectingCands/Collect_Gen_pPb2.C
+++ CollectingCands/Collect_Gen_pPb2.C
@@ -25,11 +25,11 @@
 using namespace std;
 //}}}
 
-void Collect_Gen_pPb2(const Int_t multMin = 0, const Int_t multMax = 300, const Double_t ptMin = 0, const Double_t ptMax = 30, const Double_t rapMin = -2.4, const Double_t rapMax = 2.4, const Double_t TrkptMin = 0, const Double_t TrkptMax = 1, const TString MupT = "4", const Int_t imass = 0)
+void Collect_Gen_pPb2(const Int_t multMin = 0, const Int_t multMax = 300, const Double_t ptMin = 0, const Double_t ptMax = 30, const Double_t rapMin = -2.4, const Double_t rapMax = 2.4, const Double_t TrkptMin = 0, const Double_t TrkptMax = 1, const TString MupT = "4", const TString trkptversion = "v1", const Int_t imass = 0)
 {
 //make directory{{{
 	TString mainDIR = gSystem->ExpandPathName(gSystem->pwd());
-	TString saveDIR = mainDIR + Form("/%d-%d_%d-%d_%d-%d_%d-%d_pPb2_MupT%s", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, MupT.Data());
+	TString saveDIR = mainDIR + Form("/%d-%d_%d-%d_%d-%d_%d-%d_pPb2_MupT%s_trk%s", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, MupT.Data(), trkptversion.Data());
 	void * dirp = gSystem->OpenDirectory(saveDIR.Data());
 	if(dirp) gSystem->FreeDirectory(dirp);
 	else gSystem->mkdir(saveDIR.Data(), kTRUE);
@@ -114,7 +114,7 @@ void Collect_Gen_pPb2(const Int_t multMin = 0, const Int_t multMax = 300, const 
 
 		if(multMin <= mult && mult < multMax)
 		{
-//Get collected trigger{{{
+//Get collected GEN trigger{{{
 			for(Int_t itrg = 0; itrg < Ntrg_Gen; itrg++)
 			{
 				vec_trg_Gen = (TLorentzVector*) Vec_trg_Gen->At(itrg);
@@ -130,7 +130,7 @@ void Collect_Gen_pPb2(const Int_t multMin = 0, const Int_t multMax = 300, const 
 			}
 //}}}
 
-//Get collected associator{{{
+//Get collected GEN associator{{{
 			if(TrueNtrgGen != 0)
 			{
 				for(Int_t itrk = 0; itrk < Nass_Gen; itrk++)
@@ -138,7 +138,12 @@ void Collect_Gen_pPb2(const Int_t multMin = 0, const Int_t multMax = 300, const 
 					vec_ass_Gen = (TLorentzVector*) Vec_ass_Gen->At(itrk);
 					if(vec_ass_Gen == 0) continue;
 					Double_t TrkptMintmp = 0.;
-					if(TrkptMin == 0) TrkptMintmp = 0.4;
+					if(TrkptMin == 0)
+					{
+						if(trkptversion == "v1") TrkptMintmp = 0.3;
+						else if(trkptversion == "v2") TrkptMintmp = 0.4;
+						else if(trkptversion == "v3") TrkptMintmp = 0.5;
+					}
 					else TrkptMintmp = TrkptMin;
 					if(vec_ass_Gen->Pt() >= TrkptMintmp && vec_ass_Gen->Pt() < TrkptMax)
 					{
@@ -149,7 +154,7 @@ void Collect_Gen_pPb2(const Int_t multMin = 0, const Int_t multMax = 300, const 
 			}
 //}}}
 
-//Get collected trigger{{{
+//Get collected RECO trigger{{{
 			for(Int_t itrg = 0; itrg < Ntrg_Reco; itrg++)
 			{
 				vec_trg_Reco = (TLorentzVector*) Vec_trg_Reco->At(itrg);
@@ -164,7 +169,7 @@ void Collect_Gen_pPb2(const Int_t multMin = 0, const Int_t multMax = 300, const 
 			}
 //}}}
 
-//Get collected associator{{{
+//Get collected RECO associator{{{
 			if(TrueNtrgReco != 0)
 			{
 				for(Int_t itrk = 0; itrk < Nass_Reco; itrk++)
@@ -172,7 +177,12 @@ void Collect_Gen_pPb2(const Int_t multMin = 0, const Int_t multMax = 300, const 
 					vec_ass_Reco = (TLorentzVector*) Vec_ass_Reco->At(itrk);
 					if(vec_ass_Reco == 0) continue;
 					Double_t TrkptMintmp = 0.;
-					if(TrkptMin == 0) TrkptMintmp = 0.3;
+					if(TrkptMin == 0)
+					{
+						if(trkptversion == "v1") TrkptMintmp = 0.3;
+						else if(trkptversion == "v2") TrkptMintmp = 0.4;
+						else if(trkptversion == "v3") TrkptMintmp = 0.5;
+					}
 					else TrkptMintmp = TrkptMin;
 					if(vec_ass_Reco->Pt() >= TrkptMintmp && vec_ass_Reco->Pt() < TrkptMax)
 					{
@@ -195,7 +205,7 @@ void Collect_Gen_pPb2(const Int_t multMin = 0, const Int_t multMax = 300, const 
 		}
 	}
 //store{{{
-	TFile* fout = new TFile(Form("%d-%d_%d-%d_%d-%d_%d-%d_pPb2_MupT%s/Sort_OniaTree_Gen_pPb2_PADoubleMuon_MC_%d.root", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, MupT.Data(), imass), "RECREATE");
+	TFile* fout = new TFile(Form("%d-%d_%d-%d_%d-%d_%d-%d_pPb2_MupT%s_trk%s/Sort_OniaTree_Gen_pPb2_PADoubleMuon_MC_%d.root", multMin, multMax, (int)ptMin, (int)ptMax, (int)(rapMin*10), (int)(rapMax*10), (int)TrkptMin, (int)TrkptMax, MupT.Data(), trkptversion.Data(), imass), "RECREATE");
 	fout->cd();
 	tout->Write();
 	fout->Close();
