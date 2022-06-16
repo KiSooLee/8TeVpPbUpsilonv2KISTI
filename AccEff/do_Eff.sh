84edca01adc0fb8a795ce91fd473065f79d576dd
Kisoo Lee <kilee@cern.ch>
2022-06-16 16:31:28 +0900

    TnP option modified

--- AccEff/do_Eff.sh
+++ AccEff/do_Eff.sh
@@ -3,9 +3,14 @@
 #DrawEff.C(Generation, MupT, isRW, isTnP)
 #DrawAccEff.C(Generation, MupT, isRW, isTnP)
 
-root -l -b -q 'Eff.C+(1, "3p5", false, true)'
-root -l -b -q 'Eff.C+(1, "3p5", true, true)'
-root -l -b -q 'Eff.C+(1, "3p5", true, false)'
-root -l -b -q 'DrawEff.C+(1, "3p5", false, true)'
-root -l -b -q 'DrawEff.C+(1, "3p5", true, true)'
-root -l -b -q 'DrawEff.C+(1, "3p5", true, false)'
+root -l -b -q 'Eff.C+(1, "3p5", true, 0)'
+root -l -b -q 'Eff.C+(1, "3p5", true, 1)'
+root -l -b -q 'Eff.C+(1, "3p5", true, 2)'
+root -l -b -q 'Eff.C+(1, "3p5", true, 3)'
+root -l -b -q 'Eff.C+(1, "3p5", true, 4)'
+root -l -b -q 'Eff.C+(1, "3p5", true, 5)'
+root -l -b -q 'Eff.C+(1, "3p5", false, 0)'
+#root -l -b -q 'Eff.C+(1, "10", true, true)'
+#root -l -b -q 'DrawEff.C+(1, "3p5", false, true)'
+#root -l -b -q 'DrawEff.C+(1, "3p5", true, true)'
+#root -l -b -q 'DrawEff.C+(1, "3p5", true, false)'
