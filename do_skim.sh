70657583e80edf2836bc5fb5c282c7cc0c0a5e36
Kisoo Lee <kilee@cern.ch>
2022-06-16 16:09:47 +0900

    same sign case is added

--- do_skim.sh
+++ do_skim.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-#dataskim.C(isMC, Generation, Weight, isAccRW, isEffRW, isTnP, MupT)
+#dataskim.C(isMC, Generation, Weight, isAccRW, isEffRW, isTnP, isSS, MupT)
 
-#root -l -b -q 'dataskim.C+(false, 1, true, true, true, true, "3p5")'
-#root -l -b -q 'dataskim.C+(false, 1, true, false, true, true, "3p5")'
-#root -l -b -q 'dataskim.C+(false, 1, true, true, false, true, "3p5")'
-#root -l -b -q 'dataskim.C+(false, 1, true, true, true, false, "3p5")'
-root -l -b -q 'dataskim.C+(true, 1, true, true, true, false, "3p5")'
+root -l -b -q 'dataskim.C+(false, 1, true, true, true, 0, false, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, true, 0, true, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, false, true, 0, false, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, false, 0, false, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, true, 1, false, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, true, 2, false, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, true, 3, false, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, true, 4, false, "3p5")'
+#root -l -b -q 'dataskim.C+(false, 1, true, true, true, 5, false, "3p5")'
+#root -l -b -q 'dataskim.C+(true, 2, false, true, true, true, "3p5")'
+#root -l -b -q 'dataskim.C+(true, 3, false, true, true, true, "3p5")'
