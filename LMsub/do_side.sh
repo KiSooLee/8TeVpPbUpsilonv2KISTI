4f24c8e6fd45c5d47515d6a5b7b910c78b9329e4
Kisoo Lee <kilee@cern.ch>
2022-06-17 11:07:07 +0900

    new version added

--- LMsub/do_side.sh
+++ LMsub/do_side.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
-#ZYAM.C(multMin, multMax, ptMin, ptMax, rapMin, rapMax, TrkptMin, TrkptMax, version, version2, Weight, MupT, Fine)
-#JetYieldComp.C(multMinhi, multMaxhi, multMinlow, multMaxlow, rapMin, rapMax, TrkptMin, TrkptMax, isfine, versionhi, versionlow, Weight, MupT)
+#ZYAM.C(multMin, multMax, ptMin, ptMax, rapMin, rapMax, TrkptMin, TrkptMax, version, isAccRW, isEffRW, isTnP, MupT, Fine)
+#ZYAM_trk.C(multMin, multMax, ptMin, ptMax, rapMin, rapMax, TrkptMin, TrkptMax, version, MupT, Fine)
+#JetYieldRatio.C(multMinhi, multMaxhi, multMinlow, multMaxlow, rapMin, rapMax, TrkptMin, TrkptMax, versionhi, versionlow, isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT, Fine)
 #CompJRatio.C(Fine, Weight, MupT)
 
-#multMin=("0" "40" "100" "200" "300")
-#multMax=("40" "100" "200" "300" "1500")
-#ptMin=("0" "3" "6" "10")
-#ptMax=("3" "6" "10" "30")
-#rapMin=("-24" "-12" "0" "12")
-#rapMax=("-12" "0" "12" "24")
-#TrkptMin=("0" "1" "2" "3" "4" "6")
-#TrkptMax=("1" "2" "3" "4" "6" "10")
-
-#root -l -b -q 'ZYAM.C+(0, 50, 12, 15, -2.4, 2.4, 0, 3, "v28", true, "3p5", true)'
-#root -l 'ZYAM.C+(90, 300, 10, 12, -2.4, 2.4, 0, 3, "v46", true, "3p5", false)'
-#root -l -b -q 'JetYieldRatio.C+(0, 50, 0, 45, -2.4, 2.4, 0, 3, false, "v28", "v27", true, "3p5")'
-#root -l 'JetYieldRatio.C+(90, 300, 0, 50, -2.4, 2.4, 0, 3, false, "v46", "v39", true, "3p5")'
-root -l 'JetYieldRatio.C+(90, 300, 0, 50, -2.4, 2.4, 0, 3, false, "v47", "v41", true, "3p5")'
+#root -l 'ZYAM.C+(70, 90, 12, 30, -2.4, 2.4, 0, 3, "v96", true, true, 0, "3p5", false)'
+#root -l 'ZYAM.C+(70, 300, 10, 12, -2.4, 2.4, 0, 3, "v86", true, true, 5, "3p5", false)'
+#root -l 'ZYAM.C+(70, 300, 15, 20, -2.4, 2.4, 0, 3, "v86", true, true, 5, "3p5", false)'
+#root -l 'ZYAM.C+(70, 300, 20, 30, -2.4, 2.4, 0, 3, "v86", true, true, 5, "3p5", false)'
+#root -l 'ZYAM.C+(0, 50, 10, 12, -2.4, 2.4, 0, 3, "v87", true, true, 5, "3p5", false)'
+#root -l 'ZYAM.C+(0, 35, 12, 30, -2.4, 2.4, 0, 3, "v98", true, true, 0, "3p5", false)'
+#root -l 'ZYAM.C+(0, 50, 20, 30, -2.4, 2.4, 0, 3, "v87", true, true, 5, "3p5", false)'
+#root -l -b -q 'ZYAM.C+(0, 50, 15, 20, -2.4, 2.4, 0, 3, "v87", true, false, 0, "3p5", false)'
+#root -l -b -q 'ZYAM.C+(0, 50, 15, 20, -2.4, 2.4, 0, 3, "v87", true, true, 1, "3p5", false)'
+#root -l -b -q 'ZYAM.C+(0, 50, 15, 20, -2.4, 2.4, 0, 3, "v87", true, true, 2, "3p5", false)'
+#root -l -b -q 'ZYAM.C+(0, 50, 15, 20, -2.4, 2.4, 0, 3, "v87", true, true, 3, "3p5", false)'
+#root -l -b -q 'ZYAM.C+(0, 50, 15, 20, -2.4, 2.4, 0, 3, "v87", true, true, 4, "3p5", false)'
+#root -l -b -q 'ZYAM.C+(0, 50, 15, 20, -2.4, 2.4, 0, 3, "v87", true, true, 5, "3p5", false)'
+#root -l 'ZYAM_MC.C+(true, 70, 300, 12, 15, -2.4, 2.4, 0, 3, "v86", "3p5", false)'
+#root -l 'ZYAM_MC.C+(true, 0, 50, 12, 15, -2.4, 2.4, 0, 3, "v87", "3p5", false)'
+#root -l 'ZYAM_trk.C+(70, 300, 12, 15, -2.4, 2.4, 0, 3, "v86", "3p5", false)'
+#root -l 'ZYAM_trk.C+(0, 35, 20, 30, -2.4, 2.4, 0, 3, "v98", "3p5", false)'
+#root -l 'ZYAM_trk_MC.C+(true, 70, 300, 15, 20, -2.4, 2.4, 0, 3, "v86", "3p5", false)'
+#root -l 'ZYAM_trk_MC.C+(false, 0, 50, 20, 30, -2.4, 2.4, 0, 3, "v87", "3p5", false)'
+#root -l 'GetValtest.C+(0, 50, 20, 30, -2.4, 2.4, 0, 3, "v60", true, true, true, "3p5", false)'
+#root -l 'ZYAMrange.C+(0, 50, 15, 30, -2.4, 2.4, 0, 3, "v41", true, true, true, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 35, -2.4, 2.4, 0, 3, "v86", "v98", true, true, 0, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", true, true, 0, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", false, true, 0, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", true, false, 0, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", true, true, 1, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", true, true, 2, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", true, true, 3, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", true, true, 4, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", true, true, 5, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", true, true, 0, true, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", true, true, 0, false, true, "3p5", false)'
+#root -l 'JetYieldRatio.C+(90, 300, 0, 50, -2.4, 2.4, 0, 3, "v65", "v66", true, true, true, true, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(90, 300, 0, 50, -2.4, 2.4, 0, 3, "v65", "v66", true, true, true, false, true, "3p5", false)'
+#root -l 'JetYieldRatio.C+(90, 300, 0, 50, -2.4, 2.4, 0, 3, "v65", "v66", false, true, true, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(90, 300, 0, 50, -2.4, 2.4, 0, 3, "v65", "v66", true, false, true, false, false, "3p5", false)'
+#root -l 'JetYieldRatio.C+(90, 300, 0, 50, -2.4, 2.4, 0, 3, "v65", "v66", true, true, false, false, false, "3p5", false)'
+#root -l 'JetYieldRatio_intg.C+(70, 90, 0, 50, -2.4, 2.4, 0, 3, "v96", "v87", true, true, 0, false, false, "3p5", false)'
+#root -l 'JetYieldRatio_MC.C+(true, 70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", "3p5", false)'
+root -l 'JetYieldRatio_trk.C+(70, 300, 0, 35, -2.4, 2.4, 0, 3, "v86", "v98", "3p5", false)'
+#root -l 'JetYieldRatio_trk_intg.C+(120, 300, 0, 50, -2.4, 2.4, 0, 3, "v96", "v87", "3p5", false)'
+#root -l 'JetYieldRatio_trk_MC.C+(false, 70, 300, 0, 50, -2.4, 2.4, 0, 3, "v86", "v87", "3p5", false)'
 #root -l -b -q 'CompJRatio.C+(false, true, "3p5")'
--- LMsub/do_sub.sh
+++ LMsub/do_sub.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#LMsub.C(multMinhi, multMaxhi, multMinlow, multMaxlow, rapMin, rapMax, TrkptMin, TrkptMax, maxF, bkgN, versionhi, versionlow, Weight, MupT)
+#LMsub.C(multMinhi, multMaxhi, multMinlow, multMaxlow, rapMin, rapMax, TrkptMin, TrkptMax, maxF, bkgN, AwayN, isfine, versionhi, versionlow, isAccRW, isEffRW, isTnP, SigSys, BkgSys, MupT)
+#LMsub_Par.C(multMinhi, multMaxhi, multMinlow, multMaxlow, rapMin, rapMax, TrkptMin, TrkptMax, maxF, bkgN, AwayN, isfine, versionhi, versionlow, MupT, FreePar)
+#LMsub_Jvar(multMinhi, multMaxhi, multMinlow, multMaxlow, rapMin, rapMax, TrkptMin, TrkptMax, maxF, AwayN, isfine, versionhi, versionlow, MupT)
 
 #multMin=("0" "40" "100" "200" "300")
 #multMax=("40" "100" "200" "300" "1500")
@@ -10,7 +12,32 @@
 #TrkptMin=("0" "1" "2" "3" "4" "6")
 #TrkptMax=("1" "2" "3" "4" "6" "10")
 
-#root -l -b -q 'LMsub.C+(110, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, true, "v19", "v28", true, "3p5")'
-root -l -b -q 'LMsub.C+(90, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v37", "v42", "v46", "v39", true, "3p5")'
+#root -l 'LMsub_Samev2.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v83", "v85", "v86", "v87", true, true, 0, false, false, "3p5")'
+#root -l 'LMsub.C+(70, 90, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v95", "v89", "v96", "v87", true, true, 0, false, false, "3p5")'
+#root -l 'LMsub.C+(70, 300, 0, 35, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v83", "v97", "v86", "v98", true, true, 0, false, false, "3p5")'
+root -l 'LMsub.C+(70, 300, 0, 35, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v97", "v86", "v98", true, true, 0, false, false, "3p5")'
+#root -l -b -q 'LMsub.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", true, true, 0, true, false, "3p5")'
+#root -l -b -q 'LMsub.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", true, true, 0, false, true, "3p5")'
+#root -l -b -q 'LMsub.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", false, true, 0, false, false, "3p5")'
+#root -l -b -q 'LMsub.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", true, false, 0, false, false, "3p5")'
+#root -l -b -q 'LMsub.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", true, true, 1, false, false, "3p5")'
+#root -l -b -q 'LMsub.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", true, true, 2, false, false, "3p5")'
+#root -l -b -q 'LMsub.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", true, true, 3, false, false, "3p5")'
+#root -l -b -q 'LMsub.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", true, true, 4, false, false, "3p5")'
+#root -l -b -q 'LMsub.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", true, true, 5, false, false, "3p5")'
+
+#for((ipar=0; ipar<9; ipar++))
+#do
+#	root -l -b -q 'LMsub_Par.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 2, 1, false, "v88", "v89", "v86", "v87", "3p5", '$ipar')'
+#done
+
+#root -l -b -q 'LMsub_Jvar.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 1, false, "v88", "v89", "v86", "v87", "3p5")'
+
+#root -l 'LMsub_trk.C+(false, false, 70, 300, 0, 35, -2.4, 2.4, 0, 3, 3, 1, false, "v83", "v97", "v86", "v98", "3p5")'
+#root -l 'LMsub_trk.C+(false, false, 70, 300, 0, 35, -2.4, 2.4, 0, 3, 3, 1, false, "v83", "v97", "v86", "v98", "3p5")'
+#root -l 'LMsub_trk.C+(true, false, 70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 1, false, "v90", "v91", "v86", "v87", "3p5")'
+#root -l -b -q 'LMsub_trk.C+(true, true, 70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 1, false, "v92", "v93", "v86", "v87", "3p5")'
+#root -l 'LMsub_MC.C+(70, 300, 0, 50, -2.4, 2.4, 0, 3, 3, 1, false, "v90", "v91", "v86", "v87", "3p5")'
+
 #root -l -b -q 'CompSub.C+(false, true, "3p5")'
 #root -l -b -q 'Compv2.C+(false, true, "3p5")'
